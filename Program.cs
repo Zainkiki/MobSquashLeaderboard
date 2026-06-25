88d7636981d931608eaf0f862425a72a9e216dc6
Zain kiki <zainkiki888@gmail.com>
2026-06-25 09:19:45 +0200

    Updated Cors

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors();
 var app = builder.Build();
+
+app.UseCors(policy => policy
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
 
 List<ScoreEntry> scores = new();
 
